d3040ee6c3e44b4d6dc8183ca5d0bf42cab38659
aless <alessandro22992012@gmail.com>
2022-02-12 18:24:28 +0100

    completo

--- app/src/main/java/it/motta/mbdage/service/MessagingFireBaseService.java
+++ app/src/main/java/it/motta/mbdage/service/MessagingFireBaseService.java
@@ -1,33 +1,22 @@
 package it.motta.mbdage.service;
 
+import android.annotation.SuppressLint;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
-import androidx.core.app.NotificationCompat;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
-import it.motta.mbdage.R;
-
+@SuppressLint("MissingFirebaseInstanceTokenRefresh")
 public class MessagingFireBaseService extends FirebaseMessagingService {
 
     @Override
     public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
-        Log.d("TAG", "From: " + remoteMessage.getFrom());
-
-        // Check if message contains a data payload.
-        if (remoteMessage.getData().size() > 0) {
-            NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(getApplicationContext(), "YOUR_CHANNEL_ID")
-                .setSmallIcon(R.mipmap.ic_launcher) // notification icon
-                .setContentTitle("title") // title for notification
-                .setContentText("message")// message for notification
-                .setAutoCancel(true); // clear notification after click
-        }
-
-        // Check if message contains a notification payload.
         if (remoteMessage.getNotification() != null) {
             Log.d("TAG", "Message Notification Body: " + remoteMessage.getNotification().getBody());
         }
+
     }
+
 }
